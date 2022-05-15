1c449b090a7ed8834c19c50deac077e4c5c9d286
emi <em4emkd@gmail.com>
2022-05-14 20:52:04 -0400

    1st commit

---
+++ src/com/syntax/groupproject/Main.java
@@ -0,0 +1,11 @@
+package com.syntax.groupproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=100; i ++){
+            System.out.print(i+" ");
+        }
+    }
+}
